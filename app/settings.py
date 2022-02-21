d8d47a048bc0f3a471e655d197f99ef6cc1dde49
youngwghs <young.mark@wgmail.org>
2022-02-20 21:24:34 -0600

    dd

--- app/settings.py
+++ app/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 #imports
+import django_heroku
+
 import os
 from pathlib import Path
-import django_heroku
 from django.contrib.messages import constants as messages
 
 
