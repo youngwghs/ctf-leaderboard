812b79ceb9d9b927ff7a695158655d6ad39da94d
Mark Young <young.mark@wgcloud.org>
2022-02-21 18:30:02 -0600

    dd

--- app/settings.py
+++ app/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
