28d2c7cab79d487e0f38d2eb64f2b857d70ba6da
Mark Young <young.mark@wgcloud.org>
2022-02-22 10:05:25 -0600

    dd

--- app/urls.py
+++ app/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import logout
+
 
 urlpatterns = [
     path('', include('ctf.urls')),
     path('admin/', admin.site.urls),
+    path('logout/', logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
 ]
