ef84c98b9ca1197c87846a29ea3002a10abd8328
Mark Young <young.mark@wgcloud.org>
2022-02-21 18:49:42 -0600

    dd

--- ctf/urls.py
+++ ctf/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
 from django.urls import path, include
+from django.contrib.auth import views as auth_views #import this
 
 
 urlpatterns = [
