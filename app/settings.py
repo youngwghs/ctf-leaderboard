4ff0c2f5664e3b73e9cd486439ad32ff77f604ac
Mark Young <young.mark@wgcloud.org>
2022-03-02 14:40:38 -0600

    dd

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = '...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # False if not in os.environ because of casting above
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'wgctf.herokuapp.com']
 
