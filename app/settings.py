262559e8ca7ae9ef4c95f92d28ce12b3d8ded48a
youngwghs <young.mark@wgmail.org>
2025-03-15 17:26:02 -0500

    dd

--- app/settings.py
+++ app/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = '...'
 # SECURITY WARNING: don't run with debug turned on in production!
 # False if not in os.environ because of casting above
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'wghsctf.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'wghsctf-f574b5919435.herokuapp.com']
 
 
 
